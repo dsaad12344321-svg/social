2993e218182ddb94a4cb5ba2b343db09cc0bcfbe
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-09-20 15:06:54 +0300

    Include FFmpeg binary in serverless bundle

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["ffmpeg-static"],
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/ffmpeg-static/ffmpeg"],
+  },
 };
 
 export default nextConfig;
